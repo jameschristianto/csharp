e95e8bcbb176dec10f7e7ae72bd86440ecc86d76
Mes <jameschrist0000@gmail.com>
2021-12-11 17:05:39 +0700

    C#

---
+++ ButtonAndText/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace ButtonAndText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.greet = new System.Windows.Forms.Label();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(12, 12);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(104, 32);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(122, 12);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(104, 32);
+            this.btn_disconnect.TabIndex = 1;
+            this.btn_disconnect.Text = "Disconnect";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.status.Location = new System.Drawing.Point(8, 421);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(94, 20);
+            this.status.TabIndex = 2;
+            this.status.Text = "Disconnect";
+            this.status.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // greet
+            // 
+            this.greet.AutoSize = true;
+            this.greet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.greet.Location = new System.Drawing.Point(40, 82);
+            this.greet.Name = "greet";
+            this.greet.Size = new System.Drawing.Size(0, 25);
+            this.greet.TabIndex = 3;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(232, 12);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(104, 32);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.greet);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form1";
+            this.Text = "Button and Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label greet;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ firstApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace firstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Console";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("What is your name?");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            string name = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello " + name + ", nice to meet you");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("How old are you?");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            int age = Convert.ToInt16(Console.ReadLine());
+
+            if(age > 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You are older " + Convert.ToString(age - 30) + " years than me");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You are younger " + Convert.ToString(30 - age) + " years than me");
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
